7dc2b1a3731bac0aa8267ee10c1ab4ddb2ec6654
rayviceteam-art <rayvice.team@gmail.com>
2026-08-05 18:18:15 +0530

    chore: wipe legacy frontend code from repository

--- src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://rayvice-backend.onrender.com/api/v1',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
